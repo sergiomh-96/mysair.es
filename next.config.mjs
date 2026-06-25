272a7e546d42d38623bc23eca22402c17ff06f38
sergiomh-96 <sergiomh.96@gmail.com>
2026-06-25 13:22:42 +0200

    feat: redirecciones

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,38 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+
+  async redirects() {
+    return [
+      // 1. Reglas específicas para PDFs concretos (deben ir primero)
+      {
+        source: "/wp-content/uploads/2020/04/MR01_V20.pdf",
+        destination: "/productos/mr01",
+        permanent: true,
+      },
+      {
+        source: "/wp-content/uploads/2020/04/MA35_V20.pdf",
+        destination: "/productos/ma35",
+        permanent: true,
+      },
+      {
+        source: "/wp-content/uploads/2021/08/Manual_HARMONY_radio_v1.0.pdf",
+        destination: "/productos/termostato_vesta",
+        permanent: true,
+      },
+      // 2. Reglas globales con comodines (wildcards) para atrapar otros PDFs antiguos
+      {
+        source: "/wp-content/uploads/:year/:month/:filename.pdf",
+        destination: "/productos",
+        permanent: true,
+      },
+      {
+        source: "/cloud/descargas_mysair/Catalogo/Dossier/:filename.pdf",
+        destination: "/productos",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
